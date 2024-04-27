35abca3916743bafe4957583340c801e03fe8da2
Dervine <dervine@Dervines-MacBook-Pro.local>
2024-04-27 21:29:55 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "LST Service Providers",
+  description: "List of service/product providers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full bg-gray-100">
+      <body className="h-full">{children}</body>
+    </html>
+  );
+}

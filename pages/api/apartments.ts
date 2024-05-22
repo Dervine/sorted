a39f22dc09605f8d55c1bf557e695cd1ea196ea9
Dervine <dervine@Dervines-MacBook-Pro.local>
2024-05-22 12:06:52 +0300

    db connection

--- pages/api/apartments.ts
+++ pages/api/apartments.ts
@@ -1,22 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { MongoClient } from 'mongodb';
+import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const client = new MongoClient(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
 
   try {
-    await client.connect();
-    const db = client.db('soko');
+    const client = await clientPromise;
+    const db = client.db("soko");
     const apartmentsCollection = db.collection('apartments');
     const apartments = await apartmentsCollection.find({}).toArray();
     res.status(200).json(apartments);
   } catch (error) {
     console.error('Error fetching apartments:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    await client.close();
   }
 }
